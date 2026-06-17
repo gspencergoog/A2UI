140fc43b341384bc48f9ee8720998a7537f2245c
Greg Spencer <gspencer@google.com>
2026-06-17 10:35:02 -0700

    chore(eval): update default baseline paths in measure_latency.py

--- eval/bin/measure_latency.py
+++ eval/bin/measure_latency.py
@@ -3,8 +3,8 @@ import json
 import numpy as np
 import os
 
-DIRECT_EVAL = "/Users/gspencer/code/a2ui/a2ui_express/eval/logs/latency_comparison_3_5/2026-06-17T01-52-06-00-00_a2ui-v0-9-1-eval_SYgJKCy4dZ8SdiH3CKw9Jm.eval"
-EXPRESS_EVAL = "/Users/gspencer/code/a2ui/a2ui_express/eval/logs/latency_comparison_3_5/2026-06-17T01-52-06-00-00_a2ui-v0-9-1-eval_mFBuzz4Jvd2Su97iZVSf7Z.eval"
+DIRECT_EVAL = "/Users/gspencer/code/a2ui/a2ui_express/eval/logs/baseline_gemini_3_5/2026-06-17T17-28-26-00-00_a2ui-v0-9-1-eval_DaPL73xrEZkseUykkkBEj6.eval"
+EXPRESS_EVAL = "/Users/gspencer/code/a2ui/a2ui_express/eval/logs/baseline_gemini_3_5_express/2026-06-17T17-32-10-00-00_a2ui-v0-9-1-eval_BkPqNBgUpcLT2NdVGc6LpD.eval"
 
 def analyze_latency(eval_path, name):
     print(f"Checking {name} path: {eval_path}")
